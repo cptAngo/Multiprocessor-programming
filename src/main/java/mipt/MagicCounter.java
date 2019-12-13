d94c08dc547653ef46363a32420eb2bc5c654e5f
cptAngo <leha.andreevich@yandex.ru>
2019-12-13 17:21:16 +0300

    remake MagicCounter

--- src/main/java/mipt/MagicCounter.java
+++ src/main/java/mipt/MagicCounter.java
@@ -1,22 +1,25 @@
 package mipt;
 
-public class MagicCounter implements Counter{
+import java.util.HashMap;
 
-    private volatile long value = 0;
-    private volatile boolean ifLocked = false;
+public class MagicCounter implements Counter {
+
+    public volatile HashMap<Long, Integer> threadsCount = new HashMap<>();
 
     @Override
     public void increment() {
-        while(ifLocked) {
-            Thread.yield();
+        Long threadId = Thread.currentThread().getId();
+        if (threadsCount.containsKey(threadId)) {
+            threadsCount.put(threadId, threadsCount.get(threadId) + 1);
         }
-        ifLocked = true;
-        value++;
-        ifLocked = false;
+        else {
+            threadsCount.put(threadId, 1);
+        }
     }
 
     @Override
     public long getValue() {
-        return value;
+        int sum = threadsCount.values().stream().mapToInt(Integer::intValue).sum();
+        return sum;
     }
 }
